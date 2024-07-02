2bf9ca26143f2418c91c33e7a35482d725a9ac1a
Sqvlen <kirillnesterenka@gmail.com>
2024-07-02 10:48:45 +0300

    delete fr, delete a part of menu, add text changer, CREED -> CREDO

---
+++ text-changer.js
@@ -0,0 +1,12 @@
+﻿const textElement = document.getElementsByClassName('made-in');
+const texts = ['Immersive web experiences\n Made in Ukraine', 'DIGITAL AGENCY\n Made in Ukraine'];
+let currentIndex = 0;
+
+function changeText() {
+    textElement.textContent = texts[currentIndex];
+    console.log(textElement.textContent);
+    currentIndex = (currentIndex + 1) % texts.length;
+    console.log(texts[currentIndex]);
+}
+
+setInterval(changeText, 5000);
